f47f78b3bd0926d190b0b20eec7fe31571d0dbba
kate <46572320+katelmao@users.noreply.github.com>
2024-03-30 03:05:29 +0000

    feat: match bleh2 ver

--- js/addons2.js
+++ js/addons2.js
@@ -18,7 +18,7 @@ let style_addons = [
     'bleh2-accent'
 ]
 let latest_versions = {
-    'main': 2024.0321,
+    'main': 2024.0330,
     'bleh2-oled': 2024.0320,
     'bleh2-accent': 2024.0320,
 }
