186feef3e85faa0f8613b6ed2382bd7d453ca276
kate <46572320+katelyynn@users.noreply.github.com>
2024-04-17 14:32:32 +0100

    feat: kinda reworked landing page & match ver more to come soon i think

--- js/addons2.js
+++ js/addons2.js
@@ -19,7 +19,7 @@ let style_addons = [
     'bleh2-light'
 ]
 let latest_versions = {
-    'main': 2024.0416,
+    'main': 2024.0417,
     'bleh2-oled': 2024.0415,
     'bleh2-accent': 2024.0320,
     'bleh2-light': 2024.0415
