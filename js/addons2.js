c36408ce272e0392a7464566716ba4bdc0cc377d
kate <46572320+katelyynn@users.noreply.github.com>
2024-04-29 14:28:34 +0100

    chore: bump

--- js/addons2.js
+++ js/addons2.js
@@ -5,7 +5,7 @@ let style_addons = [
     'bleh2-light'
 ]
 let latest_versions = {
-    'main': 2024.0428,
+    'main': 2024.0429,
     'bleh2-oled': 2024.0415,
     'bleh2-accent': 2024.0320,
     'bleh2-light': 2024.0415
