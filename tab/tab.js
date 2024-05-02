b1fd9b1b718f4a590271fec4b9081f1ae5bba92d
kate <46572320+katelyynn@users.noreply.github.com>
2024-05-02 16:39:37 +0100

    feat: bring back last.fm integration

--- tab/tab.js
+++ tab/tab.js
@@ -5,6 +5,26 @@ let search_type = 'web-search';
 let time_of_day = 'morning';
 
 
+let search_types = {
+    'web-search': {
+        pre: 'https://duckduckgo.com/?q=',
+        code: ''
+    },
+    'direct-url': {
+        pre: '',
+        code: '?'
+    },
+    'lastfm': {
+        pre: 'https://www.last.fm/search?q=',
+        code: '/'
+    },
+    'youtube': {
+        pre: 'https://www.youtube.com/results?search_query=',
+        code: '>'
+    }
+}
+
+
 function update_query(value) {
     let starting_val = value[0];
 
@@ -12,6 +32,8 @@ function update_query(value) {
         update_search_type('direct-url');
     } else if (starting_val == '/') {
         update_search_type('lastfm');
+    } else if (starting_val == '>') {
+        update_search_type('youtube');
     } else {
         update_search_type('web-search');
     }
@@ -26,18 +48,33 @@ function update_search_type(new_type) {
 }
 
 
+document.getElementById('search-input').addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        send_result(document.getElementById('search').value);
+    }
+});
+
+
+function send_result(value) {
+    if (value == '' || value == '?' || value == '/') return;
+
+    window.location.href = search_types[search_type].pre + value.replace(search_types[search_type].code,'');
+}
+
+
 function update_time() {
     let date = new Date();
 
     // time of day
     let date_hours = date.getHours();
-    if      (date_hours < 6)  time_of_day = 'night';
+    if      (date_hours < 7)  time_of_day = 'night';
     else if (date_hours < 12) time_of_day = 'morning';
-    else if (date_hours < 18) time_of_day = 'afternoon';
+    else if (date_hours < 16) time_of_day = 'afternoon';
+    else if (date_hours < 23) time_of_day = 'afternoon-later';
     else                      time_of_day = 'evening';
 
     document.documentElement.setAttribute('time-of-day',time_of_day);
-    document.getElementById('time-of-day').textContent = time_of_day;
+    document.getElementById('time-of-day').textContent = time_of_day.replace('-later','');
 }
 
 // interval
