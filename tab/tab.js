a75eb2a19c1e1352582502d8774c5e6ece766daa
kate <46572320+katelyynn@users.noreply.github.com>
2024-05-02 15:19:01 +0100

    feat: time of day

--- tab/tab.js
+++ tab/tab.js
@@ -2,6 +2,7 @@
 
 
 let search_type = 'web-search';
+let time_of_day = 'morning';
 
 
 function update_query(value) {
@@ -23,3 +24,21 @@ function update_search_type(new_type) {
         document.getElementById('search-input').setAttribute('data-search',new_type);
     }
 }
+
+
+function update_time() {
+    let date = new Date();
+
+    // time of day
+    let date_hours = date.getHours();
+    if      (date_hours < 6)  time_of_day = 'night';
+    else if (date_hours < 12) time_of_day = 'morning';
+    else if (date_hours < 18) time_of_day = 'afternoon';
+    else                      time_of_day = 'evening';
+
+    document.documentElement.setAttribute('time-of-day',time_of_day);
+    document.getElementById('time-of-day').textContent = time_of_day;
+}
+
+// interval
+window.setInterval(update_time, 50);
