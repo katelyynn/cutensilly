6093c84f65ee8c52fad6c26174cd46ebfb742cea
kate <46572320+katelyynn@users.noreply.github.com>
2024-05-20 20:33:33 +0100

    chore: cleanup

--- cli/js/cli.js
+++ cli/js/cli.js
@@ -29,16 +29,6 @@ let projects = {
         ],
         copyright: '2022-2024 (c) kate GPL-3.0'
     },
-    'skyplex': {
-        bio: 'a <i>WIP</i> vanilla RPG-esc, vaguely hypixel skyblock-inspired experience in minecraft - featuring professions, leveling, xp boosters, quests, shops, and more planned.',
-        links: [
-            {
-                type: 'sauce',
-                link: 'https://github.com/katelyynn/skyplex'
-            }
-        ],
-        copyright: '2022-2024 (c) kate GPL-3.0'
-    },
     'bleh': {
         bio: 'a complete visual overhaul for last.fm, incomplete but good enough at this stage. check out the site for more information and tweaks.',
         links: [
