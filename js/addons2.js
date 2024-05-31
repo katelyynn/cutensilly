a7812de57c3a67eab9286de0a8c1d8cae8ac7459
kate <46572320+katelyynn@users.noreply.github.com>
2024-05-31 22:41:23 +0100

    chore: bump

--- js/addons2.js
+++ js/addons2.js
@@ -6,9 +6,9 @@ let style_addons = [
 ]
 let latest_versions = {
     'main': 2024.0531,
-    'bleh2-oled': 2024.0415,
+    'bleh2-oled': 2024.0531,
     'bleh2-accent': 2024.0320,
-    'bleh2-light': 2024.0520
+    'bleh2-light': 2024.0531
 }
 let any_updates_available = false;
 let sent_update_notif = false;
