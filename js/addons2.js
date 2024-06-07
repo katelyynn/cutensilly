358c2ea0e5aef850a1805ddadb68fb151d1e1e83
kate <46572320+katelyynn@users.noreply.github.com>
2024-06-07 05:54:34 +0100

    feat: prepare

--- js/addons2.js
+++ js/addons2.js
@@ -1,17 +1,6 @@
 let style_addons = [
-    'main',
-    'bleh2-oled',
-    'bleh2-accent',
-    'bleh2-light'
+    'main'
 ]
-let latest_versions = {
-    'main': 2024.0606,
-    'bleh2-oled': 2024.0531,
-    'bleh2-accent': 2024.0320,
-    'bleh2-light': 2024.0603
-}
-let any_updates_available = false;
-let sent_update_notif = false;
 function query_versions() {
     any_updates_available = false;
     for (let addon in style_addons) {
@@ -20,72 +9,9 @@ function query_versions() {
     }
 
     // hide dashboard on main page
-    if (isNaN(versions.main)) {
-        document.getElementById('dashboard').style.setProperty('display','none');
-    } else {
-        document.getElementById('dashboard').style.removeProperty('display');
+    if (!isNaN(versions.main)) {
+        window.location.href = '/bleh/fm/migrate';
     }
-
-    // display version number
-    try {
-    document.getElementById('current-version').textContent = versions.main;
-    document.getElementById('latest-version').textContent = latest_versions.main;
-
-    // update action
-    if (need_updates.main == -1) {
-        document.getElementById('update-action').classList.remove('update');
-        document.getElementById('update-action').classList.add('downgrade');
-        document.getElementById('update-text').textContent = 'Downgrade';
-    } else if (need_updates.main == 0) {
-        document.getElementById('update-action').classList.remove('update');
-        document.getElementById('update-action').classList.remove('downgrade');
-        document.getElementById('update-text').textContent = 'Reinstall';
-    } else if (need_updates.main != -2) {
-        document.getElementById('update-action').classList.add('update');
-        document.getElementById('update-action').classList.remove('downgrade');
-        document.getElementById('update-text').textContent = 'Update';
-    }
-    } catch(e) {console.log(e)}
-}
-
-
-// pad version number if needed
-function pad_ver(ver) {
-    let ver_len = `${ver}`.length;
-    if (ver_len < 9) return `${ver}0`;
-    else return ver;
-}
-
-
-let versions = {};
-let need_updates = {};
-function get_version(type) {
-    if (type == 'main') {
-        versions.main = parseFloat(getComputedStyle(document.body).getPropertyValue('--bleh2-ver'));
-    } else {
-        versions[type] = parseFloat(getComputedStyle(document.body).getPropertyValue(`--${type}-ver`));
-    }
-}
-
-function check_update(type) {
-    let current = versions[type];
-    let latest = latest_versions[type];
-    let status = -2;
-
-    if (latest > current)
-        status = 1;
-    else if (current > latest)
-        status = -1;
-    else if (current == latest)
-        status = 0;
-
-    if (current == '')
-        status = -2;
-
-    if (status == 1)
-        any_updates_available = true;
-
-    need_updates[type] = status;
 }
 
 
--- js/windows.js
+++ js/windows.js
@@ -1,181 +1,54 @@
 // show pop-up windows
 
 
-let theme_windows = {
-    'winter-haters': {
-        'icon': 'snowflake',
-        'name': 'undo winter effects',
-        'description': '(limited for winter) removes the effects if u hate them',
-        'image': '/img/shiggy-chilly.png'
-    },
-    'feat': {
-        'icon': 'star',
-        'name': 'feat. artists',
-        'description': 'display featured artists in a cuter way',
-        'image': '/img/feat.png'
-    },
-    'chart': {
-        'icon': 'hash',
-        'name': 'chart colours',
-        'description': 'apply colour based on charting position',
-        'image': '/img/chart.png'
-    },
-    'bleh2-oled': {
-        'icon': 'moon-star',
-        'name': 'OLED theme',
-        'description': 'Ultra blackout.',
-        'image': '/img/bleh2-addon-oled.png'
-    },
-    'bleh2-accent': {
-        'icon': 'paintbrush',
-        'name': 'Album covers',
-        'description': 'Match the page colour to the album cover!',
-        'image': '/img/bleh2-addon-accent.png',
-        'status': 'wip'
-    },
-    'bleh2-light': {
-        'icon': 'sun',
-        'name': 'Light theme',
-        'description': 'Low saturation, brightly coloured.',
-        'image': '/img/bleh2-addon-light.png',
-        'status': 'wip'
-    }
-}
-
-
-function install_addon(type, is_script = false) {
-    let extension = 'css';
-    if (is_script) extension = 'js';
-
-    open(`https://github.com/katelyynn/bleh/raw/uwu/lastfm-${type}.user.${extension}`);
-    kill_windows();
-}
-
 function install_theme() {
-    open(`https://github.com/katelyynn/bleh/raw/uwu/lastfm-bleh2.user.css`);
+    open(`https://github.com/katelyynn/bleh/raw/uwu/fm/bleh.user.js`);
     kill_windows();
-    create_window('Install theme','Make sure to hit "Install style", then you can continue.',[
+    create_window('Installing bleh',`
+        If you have the script installed (see below), hit continue.
+        <br><br>
+        <img src="/img/install-script.png">
+        `,[
         {
             'text': 'Continue',
+            'type': 'brand',
             'onclick': `finish_theme()`
         }
-    ],'finish_theme');
+    ],'migrate_3');
 }
 function finish_theme() {
-    location.reload();
-}
-
-
-let status_texts = {
-    'alpha': 'Alpha',
-    'wip': 'WIP'
-}
-
-function create_theme_window(type,show_warning = false) {
-    try {
-        // status
-        let status = -3;
-        let status_str = '-3';
-        try {
-            status = need_updates[type];
-            status_str = status.toString();
-        } catch(e) {}
-        let btn_type = undefined;
-
-        let stt_text = '';
-        let stt_col = '';
-        let stt_id = '';
-
-        if (status == -2) {
-            stt_text = 'Not installed';
-            stt_col = 'updated';
-            stt_id = 'uninstalled';
-        } else if (status == -1) {
-            stt_text = 'Installed';
-            stt_col = 'away';
-            stt_id = 'installed_newer';
-        } else if (status == 1) {
-            stt_text = 'Update available';
-            stt_col = 'updated';
-            stt_id = 'updates_available';
-        } else if (status == 0) {
-            stt_text = 'Installed';
-            stt_col = 'offline';
-            stt_id = 'installed';
-        } else {
-            stt_text = '';
-            stt_col = 'offline';
-            stt_id = ''
-        }
-
-        if (status == 1 || status == -2)
-            btn_type = 'primary';
-
-        let html_title = `
-        <h5>${theme_windows[type].name}</h5>
-        <div class="status ${stt_col}" data-status-id="${stt_id}"><div class="status-dot"></div>${stt_text}</div>
-        `;
-        if (theme_windows[type].status != undefined)
-            html_title = `
-        <h5>${theme_windows[type].name}</h5>
-        <div class="status ${stt_col}" data-status-id="${stt_id}"><div class="status-dot"></div>${stt_text}</div>
-        <div class="badge" data-kate-type="${theme_windows[type].status}">${status_texts[theme_windows[type].status]}</div>
-        `;
-
-        let html_warning = '';
-        if (show_warning)
-            html_warning = '<h5 class="sub" data-kate-type="error">Version checking is not available for script addons.</h5>';
-
-        create_window(html_title,`
-        <p>${theme_windows[type].description}</p>
-        <br>
-        <img src="${theme_windows[type].image}" alt="${theme_windows[type].name}">
-        ${html_warning}
-        `,[
-            {
-                'text': 'Close',
-                'onclick': 'kill_windows()',
-                'type': 'mimic'
-            },
-            {
-                'text': 'Install addon',
-                'onclick': `install_addon('${type}',${show_warning})`,
-                'type': btn_type
-            }
-        ],`theme_${stt_id}_${type}`,theme_windows[type].icon,true);
-    } catch(e) {
-        create_chip(`Interface for ${type} is not implemented.`,'error');
-        console.error(e);
-    }
+    open('https://www.last.fm/bleh');
+    kill_windows();
+    create_window('Installing bleh','You may now close this tab.',[],'installation_finished');
 }
 
 
 function create_install_window2() {
-    create_window('hey!','<strong>This theme uses the Stylus browser extension.</strong><br><br>Do you have it installed?',[
+    create_window('hey!','<strong>This theme uses the Tampermonkey browser extension.</strong><br><br>Do you have it installed?',[
         {
-            'text': 'Yes, I have Stylus',
+            'text': 'Yes, I have Tampermonkey',
             'onclick': `install_theme_final()`
         },
         {
             'text': 'No, please install it',
-            'onclick': `install_stylus()`,
+            'onclick': `install_tm()`,
             'type': 'primary'
         }
     ],'install_theme');
 }
 
 
-function install_stylus() {
+function install_tm() {
     kill_windows();
-    create_window('Install Stylus',`
+    create_window('Install Tampermonkey',`
     <p>Depending on your browser, choose either option.</p>
     <div class="browser-choices">
-        <a class="btn browser" href="https://chrome.google.com/webstore/detail/stylus/clngdbkpkpeebahjckkjfobafhncgmne" target="_blank">
+        <a class="btn browser" href="https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo" target="_blank">
             <img class="browser-icon" src="/img/chrome.png">
             <p>Chrome</p>
             <p class="caption">for Chrome, Edge, Brave, Opera</p>
         </a>
-        <a class="btn browser" href="https://addons.mozilla.org/firefox/addon/styl-us/" target="_blank">
+        <a class="btn browser" href="https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/" target="_blank">
             <img class="browser-icon" src="/img/firefox.png">
             <p>Firefox</p>
             <p class="caption">for Firefox only</p>
@@ -186,25 +59,23 @@ function install_stylus() {
             'text': 'Done',
             'onclick': `install_theme_final()`
         }
-    ],'install_stylus');
+    ],'install_tm');
 }
 
 
 function install_theme_final() {
     kill_windows();
     create_window('Install theme',`
-    <p>Click the button in this window to install.</p>
-    <br>
-    <img src="/img/install-bleh2.png" alt="Screenshot showing the top-left location of the 'Install style' button">
+    Almost done! Once you have the extension installed, hit the button below to install the script.
     <br><br>
-    <p>In the tab that opens, hit "Install style" then close the tab.</p>
+    <img src="/img/install-script.png">
     `,[
         {
             'text': 'Install theme',
             'onclick': `install_theme()`,
-            'type': 'primary'
+            'type': 'brand'
         }
-    ],'install_stylus');
+    ],'finish_theme');
 }
 
 
