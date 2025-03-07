df4f2a4980d75d3cd7ca0d64fd7a99b87ec0e3f2
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-03-07 02:20:16 +0000

    feat: work list

---
+++ src/app/_components/work/work.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import style from "./work.module.css";
+
+import Image from 'next/image';
+import { KathyClicky, KathyClickyList } from '../clicky/clicky';
+
+import * as SolarIconSet from "solar-icon-set";
+
+interface KathyWorkProps {
+    image?: string,
+    title: string,
+    bio: string,
+    view_url?: WorkLink,
+    source_url?: WorkLink,
+    years: string
+}
+
+interface WorkLink {
+    link: string,
+    external?: boolean
+}
+
+export const KathyWork = ({
+    image,
+    title,
+    bio,
+    view_url,
+    source_url,
+    years
+}: KathyWorkProps) => {
+    return (
+        <li className={style.work}>
+            <div className={style.image}>
+                {image ? <Image src={image} alt={title} /> : ''}
+            </div>
+            <div className={style.info}>
+                <h4 className={style.title}>{title}</h4>
+                <p className={style.bio}>{bio}</p>
+                <p className={style.years}>{years}</p>
+                <KathyClickyList>
+                    {view_url ?
+                    <KathyClicky primary link={view_url.link} elem={view_url.external ? 'a' : 'link'}>
+                        view
+                        <SolarIconSet.DoubleAltArrowRight iconStyle="BoldDuotone" size={20} />
+                    </KathyClicky>
+                    : ''}
+                    {source_url ?
+                    <KathyClicky link={source_url.link} elem={source_url.external ? 'a' : 'link'}>
+                        source
+                        <SolarIconSet.Code2 iconStyle="BoldDuotone" size={20} />
+                    </KathyClicky>
+                    : ''}
+                </KathyClickyList>
+            </div>
+        </li>
+    );
+}
+
+export const KathyWorkList = ({ children } : { children: React.ReactNode }) => {
+    return (
+        <ul className={style.list}>
+            {children}
+        </ul>
+    );
+}
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { KathyCard, KathyCardList } from '~/app/_components/card/card';
 import { KathyQuote } from '~/app/_components/quote/quote';
 import { KathyAvatar } from '~/app/_components/avatar/avatar';
+import { KathyWork, KathyWorkList } from '../_components/work/work';
 
 export default async function Home() {
   return (
@@ -9,7 +10,32 @@ export default async function Home() {
       <h3>my <span className="kyuu">work</span></h3>
       <KathyCardList>
         <KathyCard>
-          <h4>find me anywhere</h4>
+          <KathyWorkList>
+            <KathyWork
+              title="bleh"
+              bio="an entire (cute) redesign for the music site last.fm"
+              source_url={{
+                link: "https://github.com/katelyynn/bleh",
+                external: true
+              }}
+              view_url={{
+                link: "/work/bleh"
+              }}
+              years="2022—2025"
+            />
+            <KathyWork
+              title="bwaa"
+              bio="brings last.fm back to 2012 while retaining all modern features"
+              source_url={{
+                link: "https://github.com/katelyynn/bwaa",
+                external: true
+              }}
+              view_url={{
+                link: "/work/bwaa"
+              }}
+              years="2024—2025"
+            />
+          </KathyWorkList>
         </KathyCard>
       </KathyCardList>
     </main>
