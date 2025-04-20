7766daadea6c3aecae57671c4dd70bbb62681d72
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-04-20 19:18:59 +0100

    feat: tiles?

---
+++ src/app/_components/tiles/tiles.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import style from "./tiles.module.css";
+
+interface KathyTileProps {
+    scheme?: 'red' | 'orange' | 'yellow' | 'green' | 'aqua' | 'blue' | 'purple' | 'grey',
+    name: string,
+    width?: number,
+    height?: number,
+    children: React.ReactNode
+}
+
+export const KathyTile = ({
+    scheme = 'grey',
+    name,
+    width = 2,
+    height = 2,
+    children
+}: KathyTileProps) => {
+    return (
+        <button className={`colourful ${style.tile} ${style[`scheme-${scheme}`]} ${style[`w-${width}`]} ${style[`h-${height}`]}`}>
+            <div className={style.icon}>
+                {children}
+            </div>
+            <div className={style.title}>
+                <strong>{name}</strong>
+            </div>
+        </button>
+    );
+}
+
+interface KathyTileColumnProps {
+    count?: number,
+    children: React.ReactNode
+}
+
+export const KathyTileColumn = ({
+    count = 2,
+    children
+}: KathyTileColumnProps) => {
+    return (
+        <div className={`${style.list} ${style[`count-${count}`]}`}>
+            {children}
+        </div>
+    )
+}
+
+interface KathyTileGroupProps {
+    children: React.ReactNode
+}
+
+export const KathyTileGroup = ({
+    children
+}: KathyTileGroupProps) => {
+    return (
+        <div className={style.group}>
+            {children}
+        </div>
+    )
+}
+
+interface KathyTileViewProps {
+    children: React.ReactNode
+}
+
+export const KathyTileView = ({
+    children
+}: KathyTileViewProps) => {
+    return (
+        <div className={style.view}>
+            {children}
+        </div>
+    )
+}
