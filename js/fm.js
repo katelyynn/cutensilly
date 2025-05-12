c5cc9c65a91091083d3e899d11d32a33b70c778f
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-05-12 22:41:52 +0100

    fix: fm

--- js/fm.js
+++ js/fm.js
@@ -20,7 +20,7 @@ function request_new_tracks() {
     is_requesting = true;
 
     let xhr = new XMLHttpRequest();
-    let url = 'https://api.katelyn.moe/fm/user/katelyness';
+    let url = 'https://api.katelyn.moe/fm';
     xhr.open('GET',url,true);
 
     xhr.onload = function() {
