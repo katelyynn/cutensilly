5a219d7cbfd33395b8433655224aa0eef297fff7
katelyn <46572320+katelyynn@users.noreply.github.com>
2025-08-29 06:52:55 +0100

    feat: move socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,44 +13,6 @@ export default async function Home() {
         <KathyAvatar image="/avatars/kat.jpg" alt="avatar for kathy" size="lg"/>
         <h1>hey im <span className="kyuu">katelyn</span> :3</h1>
         <p>silly lil girl who codes</p>
-        <div className="sep" />
-        <KathyClickyGrid>
-          <KathyClicky cloak="social" colour="lastdotfm" link="https://last.fm/user/clairedoll" primary elem="a">
-            <Image src="/avatars/lastdotfm.png" alt="last.fm logo" width={16} height={16}/>
-            <div className="info">
-              <strong>last.fm</strong>
-              clairedoll
-            </div>
-          </KathyClicky>
-          <KathyClicky cloak="social" colour="discogs" link="https://www.discogs.com/user/longsocks/collection" primary elem="a">
-            <Image src="/avatars/discogs.png" alt="discogs logo" width={16} height={16}/>
-            <div className="info">
-              <strong>discogs</strong>
-              longsocks
-            </div>
-          </KathyClicky>
-          <KathyClicky cloak="social" colour="github" link="https://github.com/katelyynn" primary elem="a">
-            <Image src="/avatars/github.png" alt="github logo" width={16} height={16}/>
-            <div className="info">
-              <strong>github</strong>
-              katelyynn
-            </div>
-          </KathyClicky>
-          <KathyClicky cloak="social" colour="aoty" link="https://www.albumoftheyear.org/user/clairedoll/" primary elem="a">
-            <Image src="/avatars/aoty.png" alt="AOTY logo" width={16} height={16}/>
-            <div className="info">
-              <strong>AOTY</strong>
-              clairedoll
-            </div>
-          </KathyClicky>
-          <KathyClicky cloak="social" colour="rym" link="https://rateyourmusic.com/~kateshapedbox" primary elem="a">
-            <Image src="/avatars/rym.ico" alt="rym logo" width={16} height={16}/>
-            <div className="info">
-              <strong>rym</strong>
-              kateshapedbox
-            </div>
-          </KathyClicky>
-        </KathyClickyGrid>
         <div className="sep"/>
         <KathyQuote who={{
           avatar: '/avatars/stella.png',
