a3f67fe302cff57e68695ee50f57e3d4883ecd3f
marin <46572320+katelyynn@users.noreply.github.com>
2026-07-05 20:05:10 +0100

    chore: wip for another time

---
+++ src/app/writings/page.tsx
@@ -0,0 +1,11 @@
+import { KathyCard, KathyCardList } from "../_components/card/card";
+
+export default function Writings() {
+    return (
+        <KathyCardList>
+            <KathyCard>
+                wip
+            </KathyCard>
+        </KathyCardList>
+    )
+}
