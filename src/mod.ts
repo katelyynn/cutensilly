76485332ac3f87dd0c1bb9b414b391f5a65d2b5a
marin <46572320+katelyynn@users.noreply.github.com>
2026-08-16 18:59:55 +0100

    chore: ermmm

--- src/mod.ts
+++ src/mod.ts
@@ -2,5 +2,5 @@ import { createApp } from '@404/aether';
 import { aether } from '@404/aether/server';
 
 const app = await createApp({ routesDir: './routes' });
-app.use(aether({ entrypoints: ['./routes'] }));
-app.serve({ port: 8000 });
+app.use(aether({ entrypoints: ['./src/routes'] }));
+app.serve({ port: 5000 });
--- src/routes/mod.tsx
+++ src/routes/mod.tsx
@@ -1,3 +1,3 @@
-export default () => {
+export default function Home() {
 	return <p>hi :3</p>;
-};
+}
