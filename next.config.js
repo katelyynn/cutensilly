77357270430fb35b13e3b9e703c07889be2e8d7e
marin <46572320+katelyynn@users.noreply.github.com>
2026-08-17 00:43:02 +0100

    chore: redirect

--- next.config.js
+++ next.config.js
@@ -6,35 +6,40 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  async redirects() {
-    return [
-      {
-        source: '/fm',
-        destination: 'https://bleh.katelyn.moe',
-        permanent: true,
-      },
-      {
-        source: '/fm/',
-        destination: 'https://bleh.katelyn.moe',
-        permanent: true,
-      },
-      {
-        source: '/bleh/',
-        destination: 'https://bleh.katelyn.moe',
-        permanent: true,
-      },
-      {
-        source: '/bleh/fm',
-        destination: 'https://bleh.katelyn.moe',
-        permanent: true,
-      },
-      {
-        source: '/bwaa/fm',
-        destination: '/bwaa',
-        permanent: true,
-      }
-    ];
-  },
+	async redirects() {
+		return [
+			{
+				source: "/fm",
+				destination: "https://bleh.katelyn.moe",
+				permanent: true,
+			},
+			{
+				source: "/fm/",
+				destination: "https://bleh.katelyn.moe",
+				permanent: true,
+			},
+			{
+				source: "/bleh/",
+				destination: "https://bleh.katelyn.moe",
+				permanent: true,
+			},
+			{
+				source: "/bleh/fm",
+				destination: "https://bleh.katelyn.moe",
+				permanent: true,
+			},
+			{
+				source: "/bwaa/fm",
+				destination: "/bwaa",
+				permanent: true,
+			},
+			{
+				source: "/\\~:slug",
+				destination: "https://github.com/katelyynn/:slug",
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default config;
